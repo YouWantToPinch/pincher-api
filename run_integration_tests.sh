b872df947a4d3d47758119f4ea60d904749e73df
bntrtm <git.bntrtm@tjbainter.com>
2025-11-18 13:05:37 -0600

    fix(tools): add shebang, extension to integration test script
    resolves #3

--- run_integration_tests.sh
+++ run_integration_tests.sh
@@ -1,3 +1,4 @@
+#!/bin/bash
 # run all go integration tests, pretty-printing any JSON from slog output determined by the SLOG_LEVEL environment variable
 go test -tags=integration -v ./... 2>&1 | while IFS= read -r line; do
   echo "$line" | jq . 2>/dev/null || echo "$line"
