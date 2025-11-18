7ecc4948b8d4fd32f028e7f2ddae32ae2068c4c1
bntrtm <git.bntrtm@tjbainter.com>
2025-11-18 13:40:26 -0600

    fix(api): remove client-exclusive logic
    The API should only care about transaction amounts as they relate to being measured by the smallest unit of some fiat currency, entirely agnostic to how that currency ought be formatted or otherwise displayed to a user. That's a client's job.

--- internal/api/types.go
+++ internal/api/types.go
@@ -2,21 +2,11 @@ package api
 
 import (
 	"fmt"
-	"strconv"
 	"strings"
 	"time"
 
 	"github.com/google/uuid"
 )
-
-// USD used to represent some amount in US cents.
-type Cent int64
-
-func (u Cent) Display() string {
-	s := strconv.FormatInt(int64(u), 10)
-	i := len(s) - 2
-	return s[:i] + string('.') + s[i:]
-}
 
 type BudgetMemberRole int
 
