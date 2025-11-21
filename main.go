2c830eb37fdbcf196f7b95a5e2341d53d4735c6c
bntrtm <git.bntrtm@tjbainter.com>
2025-11-21 13:24:15 -0600

    fix(api): specify port to serve on
    revert some bc17a38 changes, made in error, unrelated to fileserving.

--- main.go
+++ main.go
@@ -1,9 +1,10 @@
 package main
 
 import (
+	"fmt"
 	_ "github.com/lib/pq"
-
 	"log"
+	"log/slog"
 	"net/http"
 
 	"github.com/YouWantToPinch/pincher-api/internal/api"
@@ -11,12 +12,18 @@ import (
 
 func main() {
 	const filepathRoot = "."
+	const port = "8080"
 
 	cfg := api.LoadEnvConfig(".env")
 
 	pincher := &http.Server{
+		Addr:    ":" + port,
 		Handler: api.SetupMux(cfg),
 	}
+
+	addr := ":8080"
+	url := "http://localhost" + addr
+	slog.Info(fmt.Sprintf("Server listening at: %s", url))
 
 	// start server
 	log.Fatal(pincher.ListenAndServe())
