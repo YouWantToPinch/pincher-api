2088791bb738f5f16dab691768dd9f62da3b3ef1
bntrtm <git.bntrtm@tjbainter.com>
2026-01-11 22:30:04 -0600

    feat(api): add queries by name for transactions

--- internal/api/api_transactions_get.go
+++ internal/api/api_transactions_get.go
@@ -6,6 +6,7 @@ import (
 	"strings"
 
 	"github.com/YouWantToPinch/pincher-api/internal/database"
+	"github.com/google/uuid"
 )
 
 func (cfg *APIConfig) handleGetTransactionSplits(w http.ResponseWriter, r *http.Request) {
@@ -35,7 +36,7 @@ func (cfg *APIConfig) handleGetTransactionSplits(w http.ResponseWriter, r *http.
 }
 
 func (cfg *APIConfig) handleGetTransaction(w http.ResponseWriter, r *http.Request) {
-	getDetails := strings.HasSuffix(r.URL.String(), "/details")
+	getDetails := strings.HasSuffix(r.URL.Path, "/details")
 
 	pathTransactionID, err := parseUUIDFromPath("transaction_id", r)
 	if err != nil {
@@ -103,23 +104,7 @@ func (cfg *APIConfig) handleGetTransaction(w http.ResponseWriter, r *http.Reques
 }
 
 func (cfg *APIConfig) handleGetTransactions(w http.ResponseWriter, r *http.Request) {
-	getDetails := strings.Contains(r.URL.String(), "/details")
-
-	parsedAccountID, err := parseUUIDFromQuery("account_id", r)
-	if err != nil {
-		respondWithError(w, http.StatusBadRequest, "", err)
-		return
-	}
-	parsedCategoryID, err := parseUUIDFromQuery("category_id", r)
-	if err != nil {
-		respondWithError(w, http.StatusBadRequest, "", err)
-		return
-	}
-	parsedPayeeID, err := parseUUIDFromQuery("payee_id", r)
-	if err != nil {
-		respondWithError(w, http.StatusBadRequest, "", err)
-		return
-	}
+	getDetails := strings.HasSuffix(r.URL.Path, "/details")
 
 	parsedStartDate, err := parseDateFromQuery("start_date", r)
 	if err != nil {
@@ -132,102 +117,166 @@ func (cfg *APIConfig) handleGetTransactions(w http.ResponseWriter, r *http.Reque
 		return
 	}
 
+	accountName := r.URL.Query().Get("account_name")
+	categoryName := r.URL.Query().Get("category_name")
+	payeeName := r.URL.Query().Get("payee_name")
+
 	pathBudgetID := getContextKeyValueAsUUID(r.Context(), "budget_id")
 
-	if !getDetails {
-		dbTransactions, err := cfg.db.GetTransactions(r.Context(), database.GetTransactionsParams{
-			AccountID:  parsedAccountID,
-			CategoryID: parsedCategoryID,
-			PayeeID:    parsedPayeeID,
-			StartDate:  parsedStartDate,
-			EndDate:    parsedEndDate,
-			BudgetID:   pathBudgetID,
-		})
+	{
+		tx, err := cfg.Pool.Begin(r.Context())
 		if err != nil {
-			respondWithError(w, http.StatusInternalServerError, "could not retrieve transactions", err)
+			respondWithError(w, http.StatusInternalServerError, "", err)
 			return
 		}
+		defer tx.Rollback(r.Context())
 
-		var transactions []Transaction
-		for _, transaction := range dbTransactions {
-			transactions = append(transactions, Transaction{
-				ID:              transaction.ID,
-				CreatedAt:       transaction.CreatedAt,
-				UpdatedAt:       transaction.UpdatedAt,
-				BudgetID:        transaction.BudgetID,
-				LoggerID:        transaction.LoggerID,
-				AccountID:       transaction.AccountID,
-				TransactionType: transaction.TransactionType,
-				TransactionDate: transaction.TransactionDate,
-				PayeeID:         transaction.PayeeID,
-				Notes:           transaction.Notes,
-				Cleared:         transaction.Cleared,
+		q := cfg.db.WithTx(tx)
+
+		parsedAccountID := uuid.Nil
+		if accountName != "" {
+			parsedAccountID, err = lookupResourceIDByName(r.Context(),
+				database.GetBudgetAccountIDByNameParams{
+					AccountName: accountName,
+					BudgetID:    pathBudgetID,
+				}, q.GetBudgetAccountIDByName)
+			if err != nil {
+				respondWithError(w, http.StatusBadRequest, "could not get account id", err)
+				return
+			}
+		}
+
+		parsedCategoryID := uuid.Nil
+		if categoryName != "" {
+			parsedCategoryID, err = lookupResourceIDByName(r.Context(),
+				database.GetBudgetCategoryIDByNameParams{
+					CategoryName: categoryName,
+					BudgetID:     pathBudgetID,
+				}, q.GetBudgetCategoryIDByName)
+			if err != nil {
+				respondWithError(w, http.StatusBadRequest, "could not get category id", err)
+				return
+			}
+		}
+
+		parsedPayeeID := uuid.Nil
+		if payeeName != "" {
+			parsedPayeeID, err = lookupResourceIDByName(r.Context(),
+				database.GetBudgetPayeeIDByNameParams{
+					PayeeName: payeeName,
+					BudgetID:  pathBudgetID,
+				}, q.GetBudgetPayeeIDByName)
+			if err != nil {
+				respondWithError(w, http.StatusBadRequest, "could not get payee id", err)
+				return
+			}
+		}
+
+		if !getDetails {
+
+			dbTransactions, err := q.GetTransactions(r.Context(), database.GetTransactionsParams{
+				AccountID:  parsedAccountID,
+				CategoryID: parsedCategoryID,
+				PayeeID:    parsedPayeeID,
+				StartDate:  parsedStartDate,
+				EndDate:    parsedEndDate,
+				BudgetID:   pathBudgetID,
 			})
-		}
-
-		type rspSchema struct {
-			Transactions []Transaction `json:"transactions"`
-		}
-
-		rspPayload := rspSchema{
-			Transactions: transactions,
-		}
-
-		respondWithJSON(w, http.StatusOK, rspPayload)
-		return
-	} else {
-
-		detailedTxns, err := cfg.db.GetTransactionDetails(r.Context(), database.GetTransactionDetailsParams{
-			AccountID:  parsedAccountID,
-			CategoryID: parsedCategoryID,
-			PayeeID:    parsedPayeeID,
-			StartDate:  parsedStartDate,
-			EndDate:    parsedEndDate,
-			BudgetID:   pathBudgetID,
-		})
-		if err != nil {
-			respondWithError(w, http.StatusInternalServerError, "could not retrieve transactions", err)
-			return
-		}
-
-		var transactions []TransactionDetail
-		for _, detailedTxn := range detailedTxns {
-
-			respSplits := make(map[string]int64)
-			{
-				data := []byte(detailedTxn.Splits)
-				source := (*json.RawMessage)(&data)
-				err := json.Unmarshal(*source, &respSplits)
-				if err != nil {
-					respondWithError(w, http.StatusInternalServerError, "failure unmarshalling transaction splits", err)
-					return
-				}
+			if err != nil {
+				respondWithError(w, http.StatusInternalServerError, "could not retrieve transactions", err)
+				return
 			}
 
-			transactions = append(transactions, TransactionDetail{
-				ID:              detailedTxn.ID,
-				TransactionType: detailedTxn.TransactionType,
-				TransactionDate: detailedTxn.TransactionDate,
-				PayeeName:       detailedTxn.PayeeName,
-				BudgetName:      detailedTxn.BudgetName.String,
-				AccountName:     detailedTxn.AccountName.String,
-				LoggerName:      detailedTxn.LoggerName.String,
-				TotalAmount:     detailedTxn.TotalAmount,
-				Notes:           detailedTxn.Notes,
-				Cleared:         detailedTxn.Cleared,
-				Splits:          respSplits,
+			var transactions []Transaction
+			for _, transaction := range dbTransactions {
+				transactions = append(transactions, Transaction{
+					ID:              transaction.ID,
+					CreatedAt:       transaction.CreatedAt,
+					UpdatedAt:       transaction.UpdatedAt,
+					BudgetID:        transaction.BudgetID,
+					LoggerID:        transaction.LoggerID,
+					AccountID:       transaction.AccountID,
+					TransactionType: transaction.TransactionType,
+					TransactionDate: transaction.TransactionDate,
+					PayeeID:         transaction.PayeeID,
+					Notes:           transaction.Notes,
+					Cleared:         transaction.Cleared,
+				})
+			}
+
+			type rspSchema struct {
+				Transactions []Transaction `json:"transactions"`
+			}
+
+			rspPayload := rspSchema{
+				Transactions: transactions,
+			}
+
+			if err := tx.Commit(r.Context()); err != nil {
+				respondWithError(w, http.StatusInternalServerError, "", err)
+				return
+			}
+
+			respondWithJSON(w, http.StatusOK, rspPayload)
+			return
+
+		} else {
+			detailedTxns, err := q.GetTransactionDetails(r.Context(), database.GetTransactionDetailsParams{
+				AccountID:  parsedAccountID,
+				CategoryID: parsedCategoryID,
+				PayeeID:    parsedPayeeID,
+				StartDate:  parsedStartDate,
+				EndDate:    parsedEndDate,
+				BudgetID:   pathBudgetID,
 			})
-		}
+			if err != nil {
+				respondWithError(w, http.StatusInternalServerError, "could not retrieve transactions", err)
+				return
+			}
 
-		type rspSchema struct {
-			Transactions []TransactionDetail `json:"transactions"`
-		}
+			var transactions []TransactionDetail
+			for _, detailedTxn := range detailedTxns {
 
-		rspPayload := rspSchema{
-			Transactions: transactions,
-		}
+				respSplits := make(map[string]int64)
+				{
+					data := []byte(detailedTxn.Splits)
+					source := (*json.RawMessage)(&data)
+					err := json.Unmarshal(*source, &respSplits)
+					if err != nil {
+						respondWithError(w, http.StatusInternalServerError, "failure unmarshalling transaction splits", err)
+						return
+					}
+				}
 
-		respondWithJSON(w, http.StatusOK, rspPayload)
-		return
+				transactions = append(transactions, TransactionDetail{
+					ID:              detailedTxn.ID,
+					TransactionType: detailedTxn.TransactionType,
+					TransactionDate: detailedTxn.TransactionDate,
+					PayeeName:       detailedTxn.PayeeName,
+					BudgetName:      detailedTxn.BudgetName.String,
+					AccountName:     detailedTxn.AccountName.String,
+					LoggerName:      detailedTxn.LoggerName.String,
+					TotalAmount:     detailedTxn.TotalAmount,
+					Notes:           detailedTxn.Notes,
+					Cleared:         detailedTxn.Cleared,
+					Splits:          respSplits,
+				})
+			}
+
+			type rspSchema struct {
+				Transactions []TransactionDetail `json:"transactions"`
+			}
+
+			rspPayload := rspSchema{
+				Transactions: transactions,
+			}
+			if err := tx.Commit(r.Context()); err != nil {
+				respondWithError(w, http.StatusInternalServerError, "", err)
+				return
+			}
+
+			respondWithJSON(w, http.StatusOK, rspPayload)
+			return
+		}
 	}
 }
