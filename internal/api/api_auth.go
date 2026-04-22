784552af0f6ec1a16217eb10cce15c684ff72708
bntrtm <git.bntrtm@tjbainter.com>
2026-04-21 22:26:00 -0500

    feat(api): implement cookie-based refresh token checks
    Now the X-Auth-Transport header may be used to signal to the API whether it should look for a refresh token within an existing 'refresh_token' cookie.

--- internal/api/api_auth.go
+++ internal/api/api_auth.go
@@ -1,7 +1,10 @@
 package api
 
 import (
+	"fmt"
+	"log/slog"
 	"net/http"
+	"strings"
 	"time"
 
 	"github.com/YouWantToPinch/pincher-api/internal/auth"
@@ -70,23 +73,8 @@ func (cfg *APIConfig) handleLoginUser(w http.ResponseWriter, r *http.Request) {
 			respondWithError(w, http.StatusInternalServerError, "could not create new access token", err)
 			return
 		}
+		rspPayload := cfg.makeAuthPayload(w, r, &dbUser, accessToken, refreshToken)
 
-		type rspSchema struct {
-			User
-			Token        string `json:"token"`
-			RefreshToken string `json:"refresh_token"`
-		}
-
-		rspPayload := rspSchema{
-			User: User{
-				ID:        dbUser.ID,
-				CreatedAt: dbUser.CreatedAt,
-				UpdatedAt: dbUser.UpdatedAt,
-				Username:  dbUser.Username,
-			},
-			Token:        accessToken,
-			RefreshToken: refreshToken,
-		}
 		if err := tx.Commit(r.Context()); err != nil {
 			respondWithError(w, http.StatusInternalServerError, "", err)
 			return
@@ -96,11 +84,9 @@ func (cfg *APIConfig) handleLoginUser(w http.ResponseWriter, r *http.Request) {
 }
 
 func (cfg *APIConfig) handleCheckRefreshToken(w http.ResponseWriter, r *http.Request) {
-	returnUser := r.URL.Query().Has("with-user")
-
-	refreshToken, err := auth.GetBearerToken(r.Header)
+	refreshToken, err := FindRefreshToken(r)
 	if err != nil {
-		respondWithError(w, http.StatusBadRequest, "could not get refresh token from header", err)
+		respondWithError(w, http.StatusBadRequest, "", err)
 		return
 	}
 
@@ -116,41 +102,24 @@ func (cfg *APIConfig) handleCheckRefreshToken(w http.ResponseWriter, r *http.Req
 		return
 	}
 
-	if returnUser {
-		type rspSchema struct {
-			User
-			Token        string `json:"token"`
-			RefreshToken string `json:"refresh_token"`
-		}
-
-		rspPayload := rspSchema{
-			User: User{
-				ID:        dbUser.ID,
-				CreatedAt: dbUser.CreatedAt,
-				UpdatedAt: dbUser.UpdatedAt,
-				Username:  dbUser.Username,
-			},
-			Token:        accessToken,
-			RefreshToken: refreshToken,
-		}
-		respondWithJSON(w, http.StatusOK, rspPayload)
-	} else {
-		type rspSchema struct {
-			Token string `json:"token"`
-		}
-		rspPayload := rspSchema{
-			Token: accessToken,
-		}
-
-		respondWithJSON(w, http.StatusOK, rspPayload)
-	}
+	rspPayload := cfg.makeAuthPayload(w, r, &dbUser, accessToken, refreshToken)
+	respondWithJSON(w, http.StatusOK, rspPayload)
 }
 
 func (cfg *APIConfig) handleRevokeRefreshToken(w http.ResponseWriter, r *http.Request) {
-	refreshToken, err := auth.GetBearerToken(r.Header)
+	refreshToken, err := FindRefreshToken(r)
 	if err != nil {
-		respondWithError(w, http.StatusBadRequest, "could not get refresh token", err)
+		respondWithError(w, http.StatusBadRequest, "", err)
 		return
+	}
+	if r.Header.Get("X-Auth-Transport") == "cookie" {
+		noCookie := &http.Cookie{
+			Name:     "refresh_token",
+			Value:    "",
+			MaxAge:   -1,
+			HttpOnly: true,
+		}
+		http.SetCookie(w, noCookie)
 	}
 
 	err = cfg.db.RevokeRefreshToken(r.Context(), refreshToken)
@@ -159,4 +128,88 @@ func (cfg *APIConfig) handleRevokeRefreshToken(w http.ResponseWriter, r *http.Re
 	}
 
 	respondWithCode(w, http.StatusNoContent)
+}
+
+// makeAuthPayload prepares and returns a response body relevant to requests
+// governing management of refresh tokens. Callers may optionally omit the
+// refresh token field from the response payload, opting for an HttpOnly cookie
+// instead for browser contexts.
+func (cfg *APIConfig) makeAuthPayload(w http.ResponseWriter, r *http.Request, dbUser *database.User, accessToken, refreshToken string) any {
+	contains := func(sub string) bool { return strings.Contains(r.URL.String(), sub) }
+
+	asksForUser := (contains("/refresh") || contains("/revoke")) && r.URL.Query().Has("with-user")
+	returnUser := (asksForUser || contains("/login")) && dbUser != nil
+	forBrowser := r.Header.Get("X-Auth-Transport") == "cookie"
+
+	var user *User
+	if dbUser != nil {
+		slog.Debug("!= nil! We are doing it.")
+		user = &User{
+			ID:        dbUser.ID,
+			CreatedAt: dbUser.CreatedAt,
+			UpdatedAt: dbUser.UpdatedAt,
+			Username:  dbUser.Username,
+		}
+	}
+
+	if forBrowser {
+		cookie := &http.Cookie{
+			Name:     "refresh_token",
+			Value:    refreshToken,
+			HttpOnly: true,
+			SameSite: http.SameSiteLaxMode,
+		}
+		http.SetCookie(w, cookie)
+	}
+
+	// returned fields will vary dependent on whether the
+	// response is meant for a browser
+	if returnUser {
+		if forBrowser {
+			return struct {
+				User
+				Token string `json:"token"`
+			}{
+				User:  *user,
+				Token: accessToken,
+			}
+		} else {
+			return struct {
+				User
+				Token        string `json:"token"`
+				RefreshToken string `json:"refresh_token"`
+			}{
+				User:         *user,
+				Token:        accessToken,
+				RefreshToken: refreshToken,
+			}
+		}
+	} else {
+		return struct {
+			Token string `json:"token"`
+		}{
+			Token: accessToken,
+		}
+	}
+}
+
+// FindRefreshToken attempts to find a refresh token in
+// the Authorization header, unless the X-Auth-Transport
+// header indicates that it should search in an existing
+// refresh_token cookie.
+func FindRefreshToken(r *http.Request) (string, error) {
+	var refreshToken string
+	var err error
+	if r.Header.Get("X-Auth-Transport") != "cookie" {
+		refreshToken, err = auth.GetBearerToken(r.Header)
+		if err != nil {
+			return "", fmt.Errorf("could not get refresh token from Authorization header: %w", err)
+		}
+	} else {
+		refreshToken, err = auth.GetRefreshTokenFromCookie(r)
+		if err != nil {
+			return "", fmt.Errorf("could not get refresh token from refresh_token cookie: %w", err)
+		}
+	}
+	return refreshToken, err
 }
